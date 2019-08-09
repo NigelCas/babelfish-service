13679631b4375092630b256aa45b858a4a95ea18
NigelCas <splinterfirex@gmail.com>
2019-08-09 21:53:12 +0530

    second commit

--- src/main/java/com/trabeya/engineering/babelfish/Application.java
+++ src/main/java/com/trabeya/engineering/babelfish/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static final String API_VERSION = "0.0.01";
     public static final String BASE_CONTROLLER_PACKAGE = "com.trabeya.engineering.babelfish";
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
